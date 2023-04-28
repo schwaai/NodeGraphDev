1ff010cc72f7c88b89d9d5ad30bce080ead188d1
alethiophile <mail@alethiophile.dev>
2023-04-28 03:15:06 +0000

    First version of code to save workflows in the app

--- web/scripts/ui.js
+++ web/scripts/ui.js
@@ -1,4 +1,4 @@
-import { api } from "./api.js";
+import { api, load_saved, save_saved } from "./api.js";
 
 export function $el(tag, propsOrChildren, children) {
 	const split = tag.split(".");
@@ -442,10 +442,12 @@ export class ComfyUI {
 		this.lastQueueSize = 0;
 		this.queue = new ComfyList("Queue");
 		this.history = new ComfyList("History");
+                this.saved = new ComfyList("Saved");
 
 		api.addEventListener("status", () => {
 			this.queue.update();
 			this.history.update();
+                        this.saved.update();
 		});
 
 		const confirmClear = this.settings.addSetting({
@@ -551,9 +553,33 @@ export class ComfyUI {
 			]),
 			this.queue.element,
 			this.history.element,
+                        this.saved.element,
+                        $el("button", {
+                                id: "comfy-save-local-button",
+                                textContent: "Save",
+                                onclick: () => {
+                                        let name = prompt("Name:", "");
+                                        let list = load_saved();
+                                        const new_item = {
+                                                prompt: [
+                                                        name,
+                                                        null,
+                                                        null,
+                                                        {
+                                                                extra_pnginfo: {
+                                                                        workflow: app.graph.serialize(),
+                                                                }
+                                                        }
+                                                ],
+                                                outputs: app.nodeOutputs,
+                                        };
+                                        list[name] = new_item;
+                                        save_saved(list);
+                                },
+                        }),
 			$el("button", {
 				id: "comfy-save-button",
-				textContent: "Save",
+				textContent: "Download",
 				onclick: () => {
 					let filename = "workflow.json";
 					if (promptFilename.value) {
@@ -593,6 +619,7 @@ export class ComfyUI {
 				}
 			}}),
 		]);
+                this.saved.show();
 
 		dragElement(this.menuContainer, this.settings);
 
