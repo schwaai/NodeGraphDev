dc4e85a0772a7bbc5fcd8d7cacc571aa51d5a58a
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-06-01 09:20:38 -0400

    Ironing out server unit tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,16 +1,22 @@
+""" the server maintains the graphs remotely, and the client is just a dumb terminal that sends requests to the
+server. """
+
 import unittest
 import json
 import requests
 from test_data import raw as test_dict
 
+
 class TestExecServer(unittest.TestCase):
     def setUp(self):
-        with open("test.json", "r") as f:
-            self.test_json = json.load(f)
+        pass
 
     def test_post_prompt(self):
+        test_req_data = {
+            "graph_name": "test_api_00"
+        }
 
-        response = requests.post("http://127.0.0.1:3389/infer", json=self.test_json)
+        response = requests.post("http://127.0.0.1:3389/infer", json=json.dumps(test_req_data))
         print(response.text)
         self.assertEqual(200, response.status_code)
 
