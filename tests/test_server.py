43cc7b5b3b1d5a0c2c70b0fa1dfbf480406dfdd2
tasha <TashaSkyUp@gmail.com>
2023-06-12 18:13:21 -0400

    updated tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -13,16 +13,28 @@ class TestExecServer(unittest.TestCase):
 
     def test_post_prompt(self):
         test_req_data = {
-            "graph_name": "basic_test",
-            "infer_uuid": "test_uuid",
-            "client_prompt": "test prompt",
+            "graph_name": "test_simple_api",
+            "prompt": "test prompt",
 
         }
         # set the json header
         headers = {"Content-Type": "application/json"}
 
-        response = requests.post("http://127.0.0.1:3389/infer", json=test_req_data, headers=headers)
+        response = requests.post("http://127.0.0.1:8188/infer", json=test_req_data, headers=headers)
         print(response.text)
+        self.assertEqual(200, response.status_code)
+
+    def test_real_estate(self):
+        test_req_data = {
+            "graph_name": "test_simple_api_real_estate",
+            "prompt": "test prompt",
+
+        }
+        # set the json header
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.post("http://0.0.0.0:8188/infer", json=test_req_data, headers=headers)
+        print(f'response:{response.text}')
         self.assertEqual(200, response.status_code)
 
 
