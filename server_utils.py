ac05f9e54bd384a56683cbb4bdae2bd565429694
tasha <TashaSkyUp@gmail.com>
2023-06-12 18:13:09 -0400

    changed var name for clarity

--- server_utils.py
+++ server_utils.py
@@ -2,13 +2,13 @@ import folder_paths
 import json
 
 
-def update_json_dict(fname, data_dict=None, data_json=None):
+def update_json_dict(fname, data_dict=None, data_json_input=None):
     """Works with data_dict or data_json but not both!"""
 
-    if data_dict is None and data_json is None:
+    if data_dict is None and data_json_input is None:
         raise ValueError("data_dict or data_json must be provided")
 
-    if data_dict is not None and data_json is not None:
+    if data_dict is not None and data_json_input is not None:
         raise ValueError("data_dict or data_json must be provided, not both")
 
     # Read the json file
@@ -22,7 +22,7 @@ def update_json_dict(fname, data_dict=None, data_json=None):
     if data_dict is not None:
         json_data.update(data_dict)
     else:
-        json_data.update(json.loads(data_json))
+        json_data.update(json.loads(data_json_input))
 
     # Write the json file
     with open(fname, "w") as f:
